b689188da54dcb8c02a504d6bd7b22a48a83bb09
Ramon Egger <egger@racerfish.com>
2021-03-21 11:28:50 +0100

    Dynamic route matcher

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use Statamic\Statamic;
+use Statamic\Support\Str;
+use Statamic\Facades\Data;
+use Statamic\Facades\Site;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Statamic\Exceptions\NotFoundHttpException;
+use Statamic\Http\Controllers\API\CollectionEntriesController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +24,26 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+
+Route::get('/routes/{route}', function (Request $request, $route) {
+
+    $url = Site::current()->relativePath(
+        str_finish(str_ireplace('/api/routes', '', $request->getUri()), '/')
+    );
+
+    if ($url === '' || $route == 'home') {
+        $url = '/';
+    }
+
+    if (Str::contains($url, '?')) {
+        $url = substr($url, 0, strpos($url, '?'));
+    }
+
+    if ($data = Data::findByUri($url, Site::current()->handle())) {
+        $collection = $data->structure()->collection()->handle();
+        return (new CollectionEntriesController($request))->show($collection, $data);
+    }
+
+    throw new NotFoundHttpException;
+})->where('route', '.*');
